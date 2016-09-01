89db1d629f7d7c40aa931fe7e22f5966b70161de
DarrenChiang <nerradchiang@gmail.com>
2016-09-01 13:46:22 +0800

    Adding hw.

--- SolarSystemDirectory.py
+++ SolarSystemDirectory.py
@@ -1,29 +1,29 @@
-print("This program tells you the size of a planet and its distance from Earth.")
+print("This program tells you the size of a planet and its distance in kilometers from Earth.")
 print("Enter the name of a planet in the Solar System: ")
 
 input = input()
 
 if input == "Mercury" :
-	print("Radius:")
-	print("Distance: ") 
+	print("Diameter: 4,878")
+	print("Distance: 92,000,000") 
 if input == "Venus" :
-	print("Radius:")
-	print("Distance: ")
+	print("Diameter: 12,104")
+	print("Distance: 42,000,000")
 if input == "Earth" :
-	print("Radius:")
+	print("Diameter: 12,756")
 	print("Distance: 0")
 if input == "Mars" :
-	print("Radius:")
-	print("Distance: ")
+	print("Diameter: 6,780")
+	print("Distance: 78,000,000")
 if input == "Jupiter" :
-	print("Radius:")
-	print("Distance: ")
+	print("Diameter: 139,822")
+	print("Distance: 628,000,000")
 if input == "Saturn" :
-	print("Radius:")
-	print("Distance: ")
+	print("Diameter: 116,464")
+	print("Distance: 1,277,000,000")
 if input == "Uranus" :
-	print("Radius:")
-	print("Distance: ")
+	print("Diameter: 50,724")
+	print("Distance: 2,720,000,000")
 if input == "Neptune" :
-	print("Radius:")
-	print("Distance: ")
+	print("Diameter: 49,248")
+	print("Distance: 4,347,000,000")
