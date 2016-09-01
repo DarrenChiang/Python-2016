09b573ddb567da6342974e1b7bdc7f896ac8acd7
DarrenChiang <nerradchiang@gmail.com>
2016-09-01 08:14:23 +0800

    Work.

---
+++ SolarSystemDirectory.py
@@ -0,0 +1,22 @@
+print("Enter the name of a planet in the Solar System: ")
+
+input = input()
+
+if input == "Mercury" :
+	print("Radius:")
+	print("Distance") 
+)
+if input == "Venus" :
+	print("v")
+if input == "Earth" :
+	print("e")
+if input == "Mars" :
+	print("ma")
+if input == "Jupiter" :
+	print("j")
+if input == "Saturn" :
+	print("s")
+if input == "Uranus" :
+	print("u")
+if input == "Neptune" :
+	print("n")
